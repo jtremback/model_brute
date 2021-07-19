1594b23ae543b004a2d4062ac09092fba22dd23a
Jehan Tremback <hi@jehan.email>
2021-07-19 07:49:02 -0700

    works in a basic sense

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::collections::HashSet;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
 struct State {
     phrase: String,
     iterations: u64
@@ -17,33 +17,58 @@ fn next(state: State) -> Vec<State> {
     if state.iterations > 5 {
         return vec![]
     }
-    
-    let state = State {
-        iterations: state.iterations + 1,
-        ..state
-    };
 
     return vec![
         State {
             phrase: format!("{}{}", state.phrase, "A"),
+            iterations: state.iterations + 1,
             ..state
         },
         State {
             phrase: format!("{}{}", state.phrase, "B"),
+            iterations: state.iterations + 1,
+            ..state
+        },
+        State {
+            phrase: state.phrase,
             ..state
         }
     ]
 }
 
+fn invariant(state: State) -> bool {
+    state.phrase != "BAAB"
+}
+
 struct StateNode {
     state: State,
-    parent: usize,
+    parent: Option<usize>,
     children: Vec<usize>
 }
 
 struct StateTree {
     nodes: Vec<StateNode>,
-    seen: HashSet<State>
+}
+
+impl StateTree {
+    fn new() -> StateTree {
+        StateTree {
+            nodes: vec![]
+        }
+    }
+
+    fn get_backtrace(&self, node_idx: usize) -> Vec<State> {
+        let mut node = &self.nodes[node_idx];
+
+        let mut trace = vec![node.state.clone()];
+
+        while node.parent.is_some() {
+            node = &self.nodes[node.parent.unwrap()];
+            trace.push(node.state.clone())
+        }
+
+        trace
+    }
 }
 
 fn main() {
@@ -51,9 +76,25 @@ fn main() {
 
     let mut state_queue = vec![initial_state];
 
+    let mut seen: HashSet<State> = HashSet::new();
+
     while !state_queue.is_empty() {
         let prev_state = state_queue.pop().unwrap();
+        // If we have seen a state before, we skip it. fn next is completely deterministic depending on previous state
+        // so there is no point in running.
+        if seen.contains(&prev_state) {
+            continue;
+        }
+
+        if !invariant(prev_state.clone()) {
+            println!("Invariant violated with state: {:#?}", prev_state);
+            break;
+        }
+
         println!("{:#?}", prev_state);
-        state_queue.append(&mut next(prev_state))
+        let mut next_states = next(prev_state.clone());
+
+        state_queue.append(&mut next_states);
+        seen.insert(prev_state);
     }
 }
